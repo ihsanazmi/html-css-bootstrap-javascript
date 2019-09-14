0cf462981d101e885f2cf18ddc012286f1effe61
ihsanazmi <ihsanazmiii@gmail.com>
2019-09-14 08:50:47 +0700

    Tugas 2 Front-end

--- javascript/table.js
+++ javascript/table.js
@@ -31,6 +31,32 @@ let inputManusia = () =>{
     })
     
     document.getElementById('list-manusia').innerHTML += list
+}
 
-    
+let inputHewan = ()=>{
+    let arrHewan = []
+    let nama_hewan = document.getElementById('nama_hewan').value
+    let umur_hewan = document.getElementById('umur_hewan').value
+    let gender_hewan = document.querySelector('input[name=gender_hewan]:checked').value
+    let status_hewan = document.querySelector('input[name=status_hewan]:checked').value
+
+    arrHewan.push(
+        {
+            nama_hewan, umur_hewan, gender_hewan, status_hewan
+        }
+    )
+
+    let list_hewan = arrHewan.map(hewan =>{
+        return(
+            `
+                <tr>
+                    <td>${nama_hewan}</td>
+                    <td>${umur_hewan}</td>
+                    <td>${gender_hewan}</td>
+                    <td>${status_hewan}</td>
+                </tr>
+            `
+        )
+    })
+    document.getElementById('list-hewan').innerHTML += list_hewan
 }
